5812337a7f2122fa226b7ca4d41a81e70c9ea803
Erica and Keit Southworth <elsandkls@kidshideaway.net>
2019-04-22 17:40:47 -0400

    reorganized the repository
    Deleted the old repository and reogranized it under a new one.  There's a directory for each language or platform specific code base.  I took a Udemy class in one language and I'm porting the example game that was built in the class to multiple languages. The goal being to main consistency between the versions.

---
+++ android/app/src/main/java/net/kidshideaway/breakout_udemyclass/BreakOut_Main.java
@@ -0,0 +1,13 @@
+package net.kidshideaway.breakout_udemyclass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BreakOut_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_break_out__main);
+    }
+}
